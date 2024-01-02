0eeb61d830ad62b6aa761bbae885a211921177b0
Pelumi Oloruntegbe <oloruntegbe.pelumi@gmail.com>
2024-01-02 20:12:26 +0100

    first commits

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-red-500'>App</h1>
+  )
+}
+import React from 'react'
+
+export default function App() {
+  return (
+    <span>
+      <span>firsttttsss</span>
+    </span>
+  )
+}
